76191e1c4213a47da7b1280d8e3aa89728175a7a
Ruxo Zheng <ruxoz@just.sent.at>
2015-06-25 21:52:04 +0700

    First attempt to load DLLs.

--- Webi/Program.fs
+++ Webi/Program.fs
@@ -10,30 +10,56 @@ open Microsoft.Owin.Hosting
 let port = 9000
 
 type FilePath = string
+type StartupConfigure = IAppBuilder -> unit
 
-type Startup() =
+type Startup(configs :StartupConfigure list) =
     member x.Configuration (app :IAppBuilder) =
         let root_dir = IO.Directory.GetCurrentDirectory()
 
-        FileServerOptions( EnableDirectoryBrowsing = true,
+        let builder = FileServerOptions( EnableDirectoryBrowsing = true,
                            FileSystem = PhysicalFileSystem(root_dir)
                          )
-        |> app.UseFileServer
-        |> ignore
+                      |> app.UseFileServer
 
-let discoverDllFiles() :FilePath seq = IO.Directory.EnumerateFiles("*.dll")
+        configs |> Seq.iter (fun f -> f builder)
+
+let discoverDllFiles() :FilePath seq =
+    IO.Directory.EnumerateFiles(".", "*.dll")
+    |> Seq.map IO.Path.GetFullPath
 
 let loadDlls (files :FilePath seq) :Assembly list =
-    files |> Seq.map Assembly.LoadFile 
+    let loadAsm s =
+        printfn "Loading %s" s
+        Assembly.LoadFile s
+
+    files |> Seq.map loadAsm 
           |> Seq.toList
+
+let filterStartup (asm :Assembly list) :Type list =
+    asm
+    |> Seq.collect (fun a ->  printfn "Filtering... %s" a.FullName
+                              a.GetTypes())
+    |> Seq.filter (fun t -> t.Name = "Startup")
+    |> Seq.toList
+
+let createConfigFunc (startup_type :Type) :StartupConfigure =
+    let ctor = startup_type.GetConstructor(null)
+    let instance = ctor.Invoke(null)
+    let config_method = startup_type.GetMethod("Configure")
+    fun (app :IAppBuilder) ->
+        config_method.Invoke(instance, [|app|]) |> ignore
 
 [<EntryPoint>]
 let main argv = 
-    discoverDllFiles()
-    |> loadDlls
-    |> ignore
+    let configs = discoverDllFiles()
+                    |> loadDlls
+                    |> filterStartup
+                    |> Seq.map createConfigFunc
+                    |> Seq.toList
 
-    use server = WebApp.Start<Startup>(sprintf "http://+:%d" port)
+    let startup = Startup configs
+
+    use server = WebApp.Start(sprintf "http://+:%d" port, fun builder -> startup.Configuration builder)
     printfn "ENTER to exit..."
     Console.ReadLine() |> ignore
     0
