4bdcec1a75d9ba5b7bfeb4c051ba85ce33ad7dea
Ruxo Zheng <ruxoz@just.sent.at>
2015-06-25 21:27:35 +0700

    Rename and create first Nancy module: MainModule

---
+++ Sample/MainModule.fs
@@ -0,0 +1,16 @@
+﻿namespace Sample
+
+open Owin
+open Nancy
+open Nancy.Owin
+
+type Startup() =
+    member x.Configure (app :IAppBuilder) :unit =
+        app.UseNancy()
+        |> ignore
+
+type MainModule() = 
+    inherit NancyModule()
+
+    do  let Get = base.Get
+        Get.["/data/sample"] <- fun _ -> "Data from API!" :> obj
